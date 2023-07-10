ec0897d07aebf24a122d236ffdb4a5e346ed4be7
Sobia Abdullah <sobiaabdullah233@gmail.com>
2023-07-10 06:29:14 -0700

    initial commit

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.firstLayout')
+@section('content2')
+<div class=" col-sm-7">
+
+<div class="row">
+  <div class="col-md-4 " style=" margin-left:5px;text-allign:justify;
+   background-color:#dff0d8;border-color: #d6e9c6;width:180px;height:320px;padding-top:7px;border-radius:5px;color: #3c763d">
+  <p>Frontend development is the art of creating visually appealing and user-friendly interfaces for websites
+     and applications. It involves using HTML, CSS, and JavaScript to build responsive designs,
+     implement animations, and optimize performance.
+   </p>
+  </div>
+  <div class="col-md-4 "style="margin-left:5px;text-allign:justify; background-color:#dff0d8;
+  border-color: #d6e9c6;width:180px;height:320px;color: #3c763d;padding-top:7px;border-radius:5px;">
+    <p>PHP is a versatile and widely used scripting language for web development.
+         It powers dynamic websites and web applications, offering features like database connectivity,
+         session management, and extensive libraries.
+         Its simplicity and large community make it a popular choice among developers.</p>
+  </div>
+  <div class="col-md-4 " style="margin-left:5px;text-allign:justify;
+  background-color:#dff0d8;border-color: #d6e9c6;width:180px;height:320px;color: #3c763d;padding-top:7px;border-radius:5px;">
+  <p>Laravel is a popular PHP framework known for its elegant syntax and robust features. It simplifies web application development with built-in tools, such as routing, ORM,
+     authentication, and caching, making it a preferred choice for developers seeking efficiency and scalability.</p>
+  </div>
+</div>
+</div>
+<div class="col-md-7 card">
+    <div class="card-header">
+        <h4>Quasar</h4>
+    </div>
+<div class="card-body">
+<ul style="text-align: left;background-color: #f5f5f5;
+border: 1px solid #e3e3e3;">
+    <li>It’s based on Vue.js</li>
+<li>You get a state-of-the-art UI for your websites and apps out of the box</li>
+<li>Best support for desktop and mobile browsers  out of the box</li>
+<li>Best-in-class support for each build mode  and the best developer experience through a tight integration with our own CLI</li>
+It’s easily customizable (CSS) and extendable (JS)
+It’s the most performance-focused framework
+It’s tree-shakable automatically
+</ul>
+</div>
+</div>
+@endsection
